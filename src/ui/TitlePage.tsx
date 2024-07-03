160b6f24c104adfe224b6980a04301508be7fb6c
fathonidf <daffafathoni@gmail.com>
2024-07-03 10:35:53 +0700

    FEAT: typewriter effect and new favicon

--- src/ui/TitlePage.tsx
+++ src/ui/TitlePage.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import Blob from "@/ui/Blob";
+import Typewriter from '@/ui/Typewriter';
 
 export default function TitlePage() {
     return (
@@ -7,9 +9,17 @@ export default function TitlePage() {
             <div className="relative z-10 m-8 space-y-4 bg-white/30 backdrop-blur-lg rounded-lg">
                 <div className="p-5">
                     <h1 className="text-5xl font-bold">Daffa Mohamad Fathoni</h1>
-                    <p className="mt-2 font-bold">
-                        An undergraduate computer science student at Universitas Indonesia
-                    </p>
+                    <div className="flex justify-center">
+                        <p className="mt-2 font-bold">
+                            A computer science student aspiring in&nbsp;
+                        </p>
+                        <p className="mt-2 font-bold underline">
+                            <Typewriter />
+                        </p>
+                        <div className='mt-2 font-bold'>
+                            <span id="cursor">|</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/ui/Typewriter.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const phrases = [
+  "Software Engineering",
+  "Website Development",
+  "Frontend Development",
+  "Backend Development",
+  "Development & Operations",
+  "Classical Piano",
+];
+
+const Typewriter: React.FC = () => {
+  const [text, setText] = useState('');
+  const [curPhraseIndex, setCurPhraseIndex] = useState(0);
+
+  useEffect(() => {
+    const writeLoop = async () => {
+      for (let j = 0; j < phrases.length; j++) {
+        const curWord = phrases[j];
+
+        for (let i = 0; i <= curWord.length; i++) {
+          setText(curWord.substring(0, i));
+          await sleep(100);
+        }
+
+        await sleep(1000);
+
+        for (let i = curWord.length; i >= 0; i--) {
+          setText(curWord.substring(0, i));
+          await sleep(100);
+        }
+
+        await sleep(500);
+      }
+      
+      setCurPhraseIndex(prevIndex => (prevIndex + 1) % phrases.length);
+    };
+
+    writeLoop();
+  }, [curPhraseIndex]);
+
+  return <span>{text}</span>;
+};
+
+export default Typewriter;

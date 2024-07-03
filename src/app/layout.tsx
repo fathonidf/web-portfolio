11681f58f1a9c29ff7878db20f9991a4797a5cc4
fathonidf <daffafathoni@gmail.com>
2024-07-03 12:14:53 +0700

    REFACTOR: opengraph-image and favicon to public/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,40 @@ export const metadata: Metadata = {
     template: '%s | Daffa Fathoni',
     default: 'Daffa Fathoni',
   },
-  description: 'Daffa Mohamad Fathoni\'s Web Portfolio.',
+  description: "Daffa Mohamad Fathoni's Web Portfolio.",
+  openGraph: {
+    title: 'Daffa Fathoni',
+    description: "Daffa Mohamad Fathoni's Web Portfolio.",
+    url: 'https://yourdomain.com',
+    type: 'website',
+    images: [
+      {
+        url: '/opengraph-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Daffa Fathoni Portfolio Cover Image',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Daffa Fathoni',
+    description: "Daffa Mohamad Fathoni's Web Portfolio.",
+    images: ['/opengraph-image.jpg'],
+  },
 };
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        {/* You can include additional head elements here if needed */}
+      </head>
       <body>{children}</body>
     </html>
   );

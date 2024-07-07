fe805ed5fc6df5fe75596951aa7607414bc910cd
fathonidf <daffafathoni@gmail.com>
2024-07-07 20:08:25 +0700

    FIX: iteration key on descriptions experiences

--- src/ui/about/Experiences.tsx
+++ src/ui/about/Experiences.tsx
@@ -28,8 +28,8 @@ const Experiences: React.FC = () => {
                         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{experience.organization}</p>
 
                         <ul className="space-y-1 list-disc pl-5">
-                            {experience.descriptions.map((points) => (
-                                <li>{points}</li>
+                            {experience.descriptions.map((points, index) => (
+                                <li key={index}>{points}</li>
                             ))}
                         </ul>
                         {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-black dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">

949d5335cc3b70fd598e91d6906527878d15dd93
fathonidf <daffafathoni@gmail.com>
2024-07-07 20:05:26 +0700

    FIX: iteration key on About

--- src/ui/about/Educations.tsx
+++ src/ui/about/Educations.tsx
@@ -20,7 +20,7 @@ const Educations: React.FC = () => {
 
             <ol className="relative border-s border-gray-200 dark:border-gray-700">
                 {educations.map((education) => (
-                    <li className="mb-10 ml-6">
+                    <li key={education.major} className="mb-10 ml-6">
                         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{education.date}</time>
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-black">{education.institute}</h3>
--- src/ui/about/Experiences.tsx
+++ src/ui/about/Experiences.tsx
@@ -21,7 +21,7 @@ const Experiences: React.FC = () => {
 
             <ol className="relative border-s border-gray-200 dark:border-gray-700">
                 {experiences.map((experience) => (
-                    <li className="mb-10 ms-4">
+                    <li key={experience.position} className="mb-10 ms-4">
                         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{experience.date}</time>
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-black">{experience.position}</h3>

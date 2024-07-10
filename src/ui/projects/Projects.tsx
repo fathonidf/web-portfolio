9828eedd01de04b81bf2c7a922cdce808123e020
fathonidf <daffafathoni@gmail.com>
2024-07-10 20:42:09 +0700

    FIX: canceled dynamic color on projects

--- src/ui/projects/Projects.tsx
+++ src/ui/projects/Projects.tsx
@@ -13,8 +13,8 @@ const Projects: React.FC = () => {
                             className="rounded-t-lg"
                             src={project.image}
                             alt={project.title}
-                            width={500} // Set appropriate width
-                            height={300} // Set appropriate height
+                            width={500}
+                            height={300}
                             layout="responsive"
                         />
                     </a>
@@ -25,11 +25,8 @@ const Projects: React.FC = () => {
                         <p className="mb-3 text-sm font-normal text-gray-700">{project.description}</p>
 
                         <div className="relative inline-flex group">
-                            <div
-                                className={`absolute transition-all duration-1000 opacity-70 -inset-px bg-${project.color} rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt`}>
-                            </div>
 
-                            <a href="" className={`relative inline-flex items-center justify center px-4 py-2 transition-all duration-200 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-${project.color} focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-500 focus:text-${project.color}`}>Read more<svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <a href="" className={`relative inline-flex items-center justify-center px-4 py-2 transition-all duration-200 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-700 hover:text-white focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-500 focus:text-gray-400`}>Read more<svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg></a>
 
